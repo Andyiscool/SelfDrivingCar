f8b18361a00376688b6b81dfda38407b18d8e82c
Andy Xiao <andy.xiao@live.com>
2021-08-22 23:27:02 -0700

    Add files via upload

---
+++ NewBehaviourScriptDriving.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private GameObject Car;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Car = GetComponent<Vehicle>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * 10 * Time.deltaTime, Space.World);
+    }
+}
